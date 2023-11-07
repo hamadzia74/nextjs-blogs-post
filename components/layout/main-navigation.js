2624d234534fb35ed55912f2a3f4008173323da8
HAMAD ZIA <hamadzia74@gmail.com>
2023-11-07 22:27:23 +0500

    Added Layout and Navigation

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+function MainNavigation(props) {
+    return (
+        <header>
+            <Link href="/">
+                <a>
+                    <Logo />
+                </a>
+            </Link>
+            <nav>
+                <ul>
+                    <li><Link href="/posts">Posts</Link></li>
+                    <li><Link href="/contact">Contact</Link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default MainNavigation;
