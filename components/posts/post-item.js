56a60a81f583349108c591eac355a03c530aea1a
HAMAD ZIA <hamadzia74@gmail.com>
2023-11-14 23:43:24 +0500

    Rendering Dummy Post Data

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -10,20 +10,18 @@ function PostItem(props) {
         year: 'numeric'
     });
     const imagePath = `/images/posts/${slug}/${image}`;
-
+    const linkPath = `/posts/${slug}`;
     return (
         <li className={classes.post}>
-            <Link>
-                <a>
+            <Link href={linkPath}>
                     <div className={classes.image}>
-                        <Image src={imagePath} alt={title} width={300} height={200} />
+                        <Image src={imagePath} alt={title} width={300} height={200} layout='responsive' />
                     </div>
                     <div className={classes.content}>
                         <h3>{title}</h3>
                         <time>{formatterDate}</time>
                         <p>{excerpt}</p>
                     </div>
-                </a>
             </Link>
         </li>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,43 @@
 import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
+import FeaturedPosts from "../components/home-page/featured-posts";
+
+const DUMMY_POSTS = [
+    {
+        slug: 'getting-started-with-nextjs',
+        title: 'Getting Started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR.',
+        date: '2023-11-14'
+    },
+    {
+        slug: 'getting-started-with-nextjs2',
+        title: 'Getting Started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR.',
+        date: '2023-11-14'
+    },
+    {
+        slug: 'getting-started-with-nextjs3',
+        title: 'Getting Started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR.',
+        date: '2023-11-14'
+    },
+    {
+        slug: 'getting-started-with-nextjs4',
+        title: 'Getting Started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR.',
+        date: '2023-11-14'
+    },
+];
 
 function HomePage() {
     return (
         <Fragment>
             <Hero />
-            {/* <FeaturedPosts /> */}
+            <FeaturedPosts posts={DUMMY_POSTS} />
         </Fragment>
     )
 }
