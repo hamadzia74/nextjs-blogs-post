e57981714cccf22707c625bb53d384afddc547d3
HAMAD ZIA <hamadzia74@gmail.com>
2023-11-18 20:44:02 +0500

    Sended Data From The Client To The API Route

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,16 +1,41 @@
+import { useState } from 'react';
 import classes from './contact-form.module.css'
 
 function ContactForm() {
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredMessage, setEnteredMessage] = useState('');
+
+
+    function sendMessageHandler(event) {
+        event.preventDefault();
+
+        // optional: add client-side validation
+
+        fetch('/api/contact', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: enteredEmail,
+                name: enteredName,
+                message: enteredMessage
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+    }
     return (
         <section className={classes.contact}>
             <h1>How can I help you?</h1>
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={sendMessageHandler}>
                 <div className={classes.controls}>
                     <div className={classes.control}>
                         <label htmlFor='email'>Your Email</label>
                         <input
                             type='email'
                             id='email'
+                            value={enteredEmail}
+                            onChange={(event) => setEnteredEmail(event.target.value)}
                             required
                         />
                     </div>
@@ -19,6 +44,8 @@ function ContactForm() {
                         <input
                             type='text'
                             id='name'
+                            value={enteredName}
+                            onChange={(event) => setEnteredName(event.target.value)}
                             required
                         />
                     </div>
@@ -28,8 +55,10 @@ function ContactForm() {
                     <textarea
                         id='message'
                         rows='5'
+                        value={enteredMessage}
+                        onChange={(event) => setEnteredMessage(event.target.value)}
                         required
-                    ></textarea>
+                    />
                 </div>
 
                 <div className={classes.actions}>
