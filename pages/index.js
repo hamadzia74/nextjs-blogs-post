47026a14b0fad4237a6e76a6649dd79bb9a7c4c8
HAMAD ZIA <hamadzia74@gmail.com>
2023-11-19 16:47:45 +0500

    Added "head" Data

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,10 @@ import { getFeaturedPosts } from "../lib/posts-util";
 function HomePage(props) {
     return (
         <Fragment>
+            <Head>
+                <title>Hamad's Blog</title>
+                <meta name="description" content="I post about programming and we development." />
+            </Head>
             <Hero />
             <FeaturedPosts posts={props.posts} />
         </Fragment>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,9 +1,16 @@
+import Head from "next/head";
 import AllPosts from "../../components/posts/all-posts";
 import { getAllPosts } from "../../lib/posts-util";
 
 function AllPostsPage(props) {
     return (
-        <AllPosts posts={props.posts} />
+        <>
+            <Head>
+                <title>All Posts</title>
+                <meta name="description" content="A list of all programming-related tutorial and posts!" />
+            </Head>
+            <AllPosts posts={props.posts} />
+        </>
     )
 }
 
